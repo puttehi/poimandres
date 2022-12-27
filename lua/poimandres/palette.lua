b237a2db66caca83130a13dbda771cd143353691
puttehi <peteeez95@gmail.com>
2022-12-28 00:24:30 +0200

    First iteration of puttehi-dark

--- lua/poimandres/palette.lua
+++ lua/poimandres/palette.lua
@@ -1,25 +1,25 @@
 --- palette variants
 local variants = {
   main = { -- main palette
-    yellow = '#FFFAC2',
-    teal1 = '#5DE4C7',
-    teal2 = '#5FB3A1',
-    teal3 = '#42675A',
-    blue1 = '#89DDFF',
-    blue2 = '#ADD7FF',
-    blue3 = '#91B4D5',
-    blue4 = '#7390AA',
-    pink1 = '#FAE4FC',
-    pink2 = '#FCC5E9',
-    pink3 = '#D0679D',
-    blueGray1 = '#A6ACCD',
-    blueGray2 = '#767C9D',
-    blueGray3 = '#506477',
-    background1 = '#303340',
-    background2 = '#1B1E28',
-    background3 = '#171922',
-    text = '#E4F0FB',
-    white = '#FFFFFF',
+    yellow = '#E1B800', -- copy+visual modes, warn, buffertarget etc.
+    teal1 = '#FCE94F', -- normal mode, types, html h1h2, ts keywords,
+    teal2 = '#CC0000', -- git_text, ```, return, cssClass, ...
+    teal3 = '#42675A', -- git_rename
+    blue1 = '#89DDFF', -- insert mode, git_stage, hint, md h5+link, border, spellcap+rare
+    blue2 = '#3465A4', -- git change, md h1-h4, funct, incsearch
+    blue3 = '#69ABBC', -- link, punct, info, more, status, tab, keyword, exc, cssClass, ..
+    blue4 = '#729FCF', -- git_dirty, nontext, ts-enum
+    pink1 = '#FAE4FC', -- not used
+    pink2 = '#AD7FA8', -- telescope selection
+    pink3 = '#EE5555', -- replace, Errors
+    blueGray1 = '#555753', -- comment, bufferinac/visible, treepicker, cssTSFuncKeyword
+    blueGray2 = '#d3d7cf', -- titles
+    blueGray3 = '#454743', -- linenr, matchparen, search
+    background1 = '#2B2D2B',
+    background2 = '#1D1E1D',
+    background3 = '#0B0C0B',
+    text = '#AAAAAA',
+    white = '#EEEEEC',
     none = 'NONE',
   },
 }
